311a955f9f6c4e73c5b556b389cc836cf0b68ae7
joshblour <joshblour@hotmail.com>
2013-12-11 17:44:39 +0100

    Update ldap_access.rb

--- lib/ffcrm_ldap/ldap_access.rb
+++ lib/ffcrm_ldap/ldap_access.rb
@@ -1,23 +1,83 @@
 require 'net/ldap'
 module FfcrmLdap
-  class LDAPAccess
-    class << self
-      # Attempts to bind to LDAP using the given username and password
-      # returns true if successful, false otherwise.
-      def authenticate(uid, password)
-        ldap = connect()
-        result = ldap.bind_as(
-          :base => Config.base,
-          :login => uid,
-          :password => password)
-        return !!result
+  module LdapAdapter
+
+    def self.valid_credentials?(login, password_plaintext)
+      options = { :login => login, :password => password_plaintext }
+      resource = LdapConnect.new(options)
+      resource.authorized? ? resource : nil
+    end
+    
+    def self.get_user_details(login)
+      options = { :login => login }
+      resource = LdapConnect.new(options)
+      resource.get_user_details
+    end
+    
+    class LdapConnect
+
+      attr_reader :ldap, :login
+
+      def initialize(params = {})
+        ldap_config = YAML.load(ERB.new(File.read("#{Rails.root}/config/ldap.yml")).result)[Rails.env]
+        ldap_options = params
+        ldap_config["ssl"] = :simple_tls if ldap_config["ssl"] === true
+        ldap_options[:encryption] = ldap_config["ssl"].to_sym if ldap_config["ssl"]
+
+        @ldap = Net::LDAP.new(ldap_options)
+        @ldap.host = ldap_config["host"]
+        @ldap.port = ldap_config["port"]
+        @ldap.base = ldap_config["base"]
+        @attribute = ldap_config["attribute"]
+
+        @login = params[:login]
+        @password = params[:password]
+        @new_password = params[:new_password]
       end
 
-      def get_user_details(uid)
-        ldap = connect()
+      def dn
+        puts("LDAP dn lookup: #{@attribute}=#{@login}")
+        ldap_entry = search_for_login
+        if ldap_entry.nil?
+          "#{login}@group-ips.local"
+        else
+          ldap_entry.dn
+        end
+      end
 
-        results = ldap.search(
-          :filter => Net::LDAP::Filter.eq(Config.attribute.to_s, uid.to_s) )
+      def authenticate!
+        @ldap.auth(dn, @password)
+        @ldap.bind
+      end
+
+      def authenticated?
+        authenticate!
+      end
+
+      def authorized?
+        puts("Authorizing user #{dn}")
+        authenticated?
+      end
+
+      def ldap_entry
+        search_for_login
+      end
+
+      # Searches the LDAP for the login
+      #
+      # @return [Object] the LDAP entry found; nil if not found
+      def search_for_login
+        puts("LDAP search for login: #{@attribute}=#{@login}")
+        filter = Net::LDAP::Filter.eq(@attribute.to_s, @login.to_s)
+        ldap_entry = nil
+        @ldap.search(:filter => filter) {|entry| ldap_entry = entry}
+        ldap_entry
+      end
+      
+      def get_user_details
+
+        results = @ldap.search(
+          :filter => Net::LDAP::Filter.eq(@attribute.to_s, @login.to_s)
         if results and results.size > 0
           details = {}
           results[0].each do |name, values|
@@ -29,35 +89,7 @@ module FfcrmLdap
         end
       end
 
-      protected
-
-      def connect()
-        return Net::LDAP.new(
-          :host => Config.host,
-          :port => Config.port,
-          :base => Config.base,
-          :encryption => Config.ssl ? :simple_tls : nil #,
-          # :auth => {
-          #         :method => :simple,
-          #         :username => Config.bind_dn,
-          #         :password => Config.bind_passwd }
-          )
-      end
     end
 
-    class Config
-      def self.method_missing(name, *args)
-        if config.has_key?(name.to_s)
-          config[name.to_s]
-        else
-          super
-        end
-      end
-
-      @config = nil
-      def self.config
-        @config ||= YAML.load_file(File.join(Rails.root, %w(config ldap.yml)))[Rails.env]
-      end
-    end
   end
 end
